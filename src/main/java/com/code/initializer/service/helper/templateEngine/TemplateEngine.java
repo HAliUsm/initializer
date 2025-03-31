6efadbf69e27455809b618d50098da40a0882eda
haliusm <h.usmanisiaka@gmail.com>
2025-03-30 20:40:35 -0700

    paserBuilt

--- src/main/java/com/code/initializer/service/helper/templateEngine/TemplateEngine.java
+++ src/main/java/com/code/initializer/service/helper/templateEngine/TemplateEngine.java
@@ -1,13 +1,14 @@
 package com.code.initializer.service.helper.templateEngine;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class TemplateEngine {
 
-    public void build(Path Template, Path OutputPath) {
+    public void build(Path template, Path OutputPath) throws IOException {
         // Implementation for building the template
-        String TemplatContent = new String(Files.readAllBytes(temple))
+        String TemplatContent = new String(Files.readAllBytes(template));
         // This is where you would process the template files and generate the output
     }
 }
